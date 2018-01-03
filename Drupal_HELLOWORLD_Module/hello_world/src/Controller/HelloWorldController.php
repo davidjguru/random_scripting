6e549f055752288205fbc1fd37827ff56ad41c5b
davidjguru <davidjguru@gmail.com>
2018-01-03 23:55:05 +0100

    Example with a initial Drupal 8 module A simple exercise for building a Hello World module in Drupal 8.

---
+++ Drupal_HELLOWORLD_Module/hello_world/src/Controller/HelloWorldController.php
@@ -0,0 +1,28 @@
+<?php
+  namespace Drupal\hello_world\Controller;
+
+  use Drupal\Core\Controller\ControllerBase;
+ 
+/*
+ * Controller for the salutation message.
+ *
+ */
+
+
+
+class HelloWorldController extends ControllerBase {
+
+/*
+ * Hello World.
+ *
+ * @return string
+ */
+
+  public function helloWorld() {
+        return [
+             '#markup' => $this->t('Hello World')
+               ];
+        }
+
+
+}
